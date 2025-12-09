ab08997ca05986da84a92b46980005546422134c
vuquanghuy482 <quanghuyvu209@gmail.com>
2025-12-09 23:01:07 +0700

    add res

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <>
             <h1>Vite + React</h1>
-            <div className="text-xl font-bold">
+            <div className="text-xl font-bold text-yellow-300">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
