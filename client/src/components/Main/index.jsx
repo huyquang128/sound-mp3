d83de89df24ce86ec00c13af9b2c5d0c6615b3f6
vuquanghuy482 <quanghuyvu209@gmail.com>
2025-12-12 23:43:23 +0700

    sidebar-listSong done

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,9 +1,13 @@
 import CarouselSong from './Carousel-Song';
+import SliderIntro from './Slider_Intro';
+import SongSuggest from './Song_Suggest';
 
 function Main() {
     return (
         <div className="mt-[47px] ml-60 bg-home-bg h-[1000px] px-14 pt-10">
             <CarouselSong />
+            <SliderIntro/>
+            <SongSuggest/>
         </div>
     );
 }
